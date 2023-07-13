ba820932700a4d0a9293faefd541e1abb7e13eb5
Ali Fotouhi <mr.ali.fotouhi@gmail.com>
2023-07-13 18:48:42 +0330

    Update permissions

--- config/settings.py
+++ config/settings.py
@@ -126,6 +126,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.BasicAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ]
 }
 
