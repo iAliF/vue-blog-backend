45023ff15132e77c7aef56eaf606feeed4d4e74a
Ali Fotouhi <mr.ali.fotouhi@gmail.com>
2023-07-28 03:23:31 +0330

    Add 'author' field to Article model

--- api/models.py
+++ api/models.py
@@ -1,8 +1,10 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
 # Create your models here.
 class Article(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=250)
     slug = models.SlugField()
     description = models.TextField()
