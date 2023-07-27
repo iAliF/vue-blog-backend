542be06d7dc90e57042cf56f5178f4c431c51039
Ali Fotouhi <mr.ali.fotouhi@gmail.com>
2023-07-28 02:21:54 +0330

    add ordering filter to article view

--- api/views.py
+++ api/views.py
@@ -1,5 +1,5 @@
 from rest_framework import viewsets
-from rest_framework.filters import SearchFilter
+from rest_framework.filters import SearchFilter, OrderingFilter
 
 from .models import Article
 from .serializers import ArticleSerializers
@@ -9,5 +9,6 @@ class ArticleViewSet(viewsets.ModelViewSet):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializers
     lookup_field = "slug"
-    filter_backends = (SearchFilter,)
+    filter_backends = (SearchFilter, OrderingFilter)
     search_fields = ("title", "slug")
+    ordering_fields = ("title", "created_at")
