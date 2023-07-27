0651f1ac0d023a0415253fca6f515ebf2b7284e1
Ali Fotouhi <mr.ali.fotouhi@gmail.com>
2023-07-28 03:27:41 +0330

    Update article admin model

--- api/admin.py
+++ api/admin.py
@@ -8,7 +8,8 @@ from .models import Article
 class ArticleAdmin(admin.ModelAdmin):
     search_fields = ('title', 'slug')
     search_help_text = "Enter title or slug to search"
-    list_display = ('pk', 'title', 'created_at', 'updated_at')
+    list_display = ('author', 'title', 'created_at', 'updated_at')
+    list_filter = ('author__username',)
     sortable_by = ('created_at', 'updated_at')
     prepopulated_fields = {
         'slug': ('title',)
