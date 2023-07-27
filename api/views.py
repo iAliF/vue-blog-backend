aff9c9d103e82f8b42238738123245a65216892e
Ali Fotouhi <mr.ali.fotouhi@gmail.com>
2023-07-28 02:10:50 +0330

    add search filter to article view

--- api/views.py
+++ api/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.filters import SearchFilter
 
 from .models import Article
 from .serializers import ArticleSerializers
@@ -8,3 +9,5 @@ class ArticleViewSet(viewsets.ModelViewSet):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializers
     lookup_field = "slug"
+    filter_backends = (SearchFilter,)
+    search_fields = ("title", "slug")
